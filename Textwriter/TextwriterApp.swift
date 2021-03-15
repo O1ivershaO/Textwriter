b0dc54fb3319f4b1ef613420c40302240daed397
Oliver Shao <olivershaw119@gmail.com>
2021-03-15 03:37:16 -0400

    Initial Commit

---
+++ Textwriter/TextwriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextwriterApp.swift
+//  Textwriter
+//
+//  Created by Oliver Shaw on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct TextwriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
